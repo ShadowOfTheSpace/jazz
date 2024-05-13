69ec580f452da8c0671a1316899aaa355bafe965
TeaChan <valkova.tanya17@gmail.com>
2024-05-13 05:26:34 +0300

    Update Button component

--- src/libs/components/button/button.tsx
+++ src/libs/components/button/button.tsx
@@ -17,16 +17,21 @@ const Button: React.FC<Properties> = ({
   onClick,
   title,
 }) => {
+  const hasOnlyIcon = Boolean(iconName && !label);
+
   return (
     <button
       className={cn(
-        "flex gap-x-[5px] border-[3px] border-jz-gold bg-jz-gold has-hover:hover:bg-transparent no-hover:active:bg-transparent px-[15px] sm:px-[32px] py-[5px] sm:py-[10px] rounded-[40px] font-inter sm:font-bold text-[16px] text-jz-main sm:text-[18px] xl:text-[20px] has-hover:hover:text-jz-gold no-hover:active:text-jz-gold transition-colors justify-center items-center",
+        "flex gap-x-[5px] border-[3px] border-jz-gold bg-jz-gold has-hover:hover:bg-transparent no-hover:active:bg-transparent  rounded-[40px] font-inter sm:font-bold text-[16px] text-jz-main sm:text-[18px] xl:text-[20px] has-hover:hover:text-jz-gold no-hover:active:text-jz-gold transition-colors justify-center items-center",
+        !hasOnlyIcon && "px-[15px] sm:px-[32px] py-[5px] sm:py-[10px]",
         className
       )}
       onClick={onClick}
       title={title}
     >
-      {iconName && <Icon name={iconName} className="shrink-0 size-[20px] sm:size-[24px]" />}
+      {iconName && (
+        <Icon name={iconName} className="shrink-0 size-[20px] sm:size-[24px]" />
+      )}
       {label && <span>{label}</span>}
     </button>
   );
