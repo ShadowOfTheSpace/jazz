0c670d1283f1374f9134c3bbc7db81d1837fc383
Oleksandr Tkachuk <oleksandr.vpro@gmail.com>
2024-05-26 03:43:40 +0300

    Add IMAGE_API_LINK constant and its usage

---
+++ src/libs/constants/constants.ts
@@ -0,0 +1,1 @@
+export { IMAGE_API_LINK } from "./image-api-link/image-api-link.constant";
---
+++ src/libs/constants/image-api-link/image-api-link.constant.ts
@@ -0,0 +1,4 @@
+const IMAGE_API_LINK =
+  "https://script.google.com/macros/s/AKfycbwHZ8s5txFxEfl-e3ixOWQ-0Y9-lTKlrgddMSnLwT4dLYeIp4iMM2TKdIZs8NDL3yaj/exec";
+
+export { IMAGE_API_LINK };
