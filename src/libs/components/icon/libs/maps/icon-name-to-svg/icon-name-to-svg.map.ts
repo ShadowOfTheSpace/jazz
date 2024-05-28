1a436aab7e5cec682966ed192395337ef59d5932
TeaChan <valkova.tanya17@gmail.com>
2024-05-28 06:19:01 +0300

    Add Tinder, Patreon and BuyMeACoffee icons

--- src/libs/components/icon/libs/maps/icon-name-to-svg/icon-name-to-svg.map.ts
+++ src/libs/components/icon/libs/maps/icon-name-to-svg/icon-name-to-svg.map.ts
@@ -1,12 +1,15 @@
 import BurgerIcon from "~assets/icons/burger.svg?react";
+import BuyMeACoffeeIcon from "~assets/icons/buy-me-a-coffee.svg?react";
 import CloseIcon from "~assets/icons/close.svg?react";
 import FacebookIcon from "~assets/icons/facebook.svg?react";
 import InstagramIcon from "~assets/icons/instagram.svg?react";
 import LeftArrowIcon from "~assets/icons/left-arrow.svg?react";
+import PatreonIcon from "~assets/icons/patreon.svg?react";
 import PauseIcon from "~assets/icons/pause.svg?react";
 import PhoneIcon from "~assets/icons/phone.svg?react";
 import PlayIcon from "~assets/icons/play.svg?react";
 import RightArrowIcon from "~assets/icons/right-arrow.svg?react";
+import TinderIcon from "~assets/icons/tinder.svg?react";
 import YouTubeIcon from "~assets/icons/youtube.svg?react";
 
 import { IconName } from "~/libs/types/types";
@@ -16,14 +19,17 @@ const iconNameToSvg: Record<
   React.FC<React.SVGProps<SVGSVGElement>>
 > = {
   burger: BurgerIcon,
+  buyMeACoffee: BuyMeACoffeeIcon,
   close: CloseIcon,
   facebook: FacebookIcon,
   instagram: InstagramIcon,
   leftArrow: LeftArrowIcon,
+  patreon: PatreonIcon,
   pause: PauseIcon,
   phone: PhoneIcon,
   play: PlayIcon,
   rightArrow: RightArrowIcon,
+  tinder: TinderIcon,
   youtube: YouTubeIcon,
 };
 
--- src/libs/types/icon-name/icon-name.type.ts
+++ src/libs/types/icon-name/icon-name.type.ts
@@ -1,13 +1,16 @@
 type IconName =
   | "burger"
+  | "buyMeACoffee"
   | "close"
   | "facebook"
   | "instagram"
   | "leftArrow"
+  | "patreon"
   | "pause"
   | "phone"
   | "play"
   | "rightArrow"
+  | "tinder"
   | "youtube";
 
 export { type IconName };
