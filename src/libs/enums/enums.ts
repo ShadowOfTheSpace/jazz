6206583cd77e70dcd82461b3cb57aaba1bb0bc6f
TeaChan <valkova.tanya17@gmail.com>
2024-06-26 08:18:15 +0300

    Rename files

--- src/libs/enums/enums.ts
+++ src/libs/enums/enums.ts
@@ -1,3 +1,3 @@
 export { AppLanguage } from "./app-language/app-language.enum";
-export { AppRoute } from "./app-route/app-route";
-export { StorageItemName } from "./storage-item-name/storage-item-name";
+export { AppRoute } from "./app-route/app-route.enum";
+export { StorageItemName } from "./storage-item-name/storage-item-name.enum";
--- src/libs/hooks/hooks.ts
+++ src/libs/hooks/hooks.ts
@@ -1,8 +1,8 @@
 export { useAppForm } from "./use-app-form/use-app-form.hook";
-export { useCartContext } from "./use-cart-context/use-cart-context";
-export { useCart } from "./use-cart/use-cart";
+export { useCartContext } from "./use-cart-context/use-cart-context.hook";
+export { useCart } from "./use-cart/use-cart.hook";
 export { useHandleEscPress } from "./use-handle-esc-press/use-handle-esc-press.hook";
-export { useImageCache } from "./use-image-cache/use-image-cache";
-export { useLanguageContext } from "./use-language-context/use-language-context";
-export { useScrollLock } from "./use-scroll-lock/use-scroll-lock";
-export { useTranslate } from "./use-translate/use-translate";
+export { useImageCache } from "./use-image-cache/use-image-cache.hook";
+export { useLanguageContext } from "./use-language-context/use-language-context.hook";
+export { useScrollLock } from "./use-scroll-lock/use-scroll-lock.hook";
+export { useTranslate } from "./use-translate/use-translate.hook";
