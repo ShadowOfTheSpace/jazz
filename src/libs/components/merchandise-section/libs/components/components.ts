92d12ba94331746cb1dd35748bffd67b71954cad
TeaChan <valkova.tanya17@gmail.com>
2024-06-28 12:39:15 +0300

    Delete unused components

--- src/libs/components/merchandise-section/libs/components/components.ts
+++ src/libs/components/merchandise-section/libs/components/components.ts
@@ -1,7 +1,5 @@
+export { CartButton } from "./cart-button/cart-button";
 export { MerchandiseCarousel } from "./merchandise-carousel/merchandise-carousel";
-export { MerchandiseItemsCount } from "./merchandise-items-count/merchandise-items-count";
 export { MerchandiseModal } from "./merchandise-modal/merchandise-modal";
-export { MerchandiseSizeSelector } from "./merchandise-size-selector/merchandise-size-selector";
 export { MerchandiseSkeleton } from "./merchandise-skeleton/merchandise-skeleton";
 export { Merchandise } from "./merchandise/merchandise";
-export { CartButton } from "./cart-button/cart-button";
