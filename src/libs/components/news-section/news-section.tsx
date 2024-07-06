2cebda0dcc63ac1510d138990884c79f3ee09829
TeaChan <valkova.tanya17@gmail.com>
2024-07-06 14:04:26 +0300

    Update news feed id

--- src/libs/components/news-section/news-section.tsx
+++ src/libs/components/news-section/news-section.tsx
@@ -24,7 +24,7 @@ const NewsSection: React.FC = () => {
         {translate("Life Vibes")}
       </Animated.Title>
       <div className="w-full [@media(max-width:351px)]:aspect-[1/2] [@media(min-width:352px)_and_(max-width:510px)]:aspect-square [@media(min-width:511px)_and_(max-width:703px)]:aspect-[3/2] [@media(min-width:704px)_and_(max-width:863px)]:aspect-[4/2] [@media(min-width:864px)_and_(max-width:1023px)]:aspect-[5/2] [@media(min-width:1024px)_and_(max-width:1183px)]:aspect-[6/2] [@media(min-width:1184px)_and_(max-width:1343px)]:aspect-[7/2] [@media(min-width:1344px)]:aspect-[8/2] overflow-hidden">
-        <Feed feedId="dd40e0a5-e345-4159-b754-877b2c90ebf8" />
+        <Feed feedId="54927234-ba36-4c1c-a0d7-69f248d3b553" />
       </div>
     </section>
   );
